3330fa94fb319c18c84b45531c2a0914839650c9
alexzandermatveev <alexzander20011@gmail.com>
2024-11-24 16:24:22 +0300

    SecurityConfig improved

--- src/main/java/com/example/hotel_app/config/SecurityConfig.java
+++ src/main/java/com/example/hotel_app/config/SecurityConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -45,8 +46,19 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(HttpSecurity http,
+                                                       CustomUserDetailsService customUserDetailsService,
+                                                       PasswordEncoder passwordEncoder) throws Exception {
+
+        var authManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+//        authManagerBuilder.userDetailsService(customUserDetailsService);
+
+        var authProvider = new DaoAuthenticationProvider(passwordEncoder);
+        authProvider.setUserDetailsService(customUserDetailsService);
+        authManagerBuilder.authenticationProvider(authProvider);
+
+        return authManagerBuilder.build();
+
     }
 
 }
